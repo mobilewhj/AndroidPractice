0a6a34464be4f7216c1b4988c4603118cfe6103a
mobilewhj <mobilewhj@gmail.com>
2023-03-27 23:43:59 +0800

    准备开始练习.

---
+++ app/src/main/java/com/mobile/practice/customview/drawing/DashboardView.kt
@@ -0,0 +1,14 @@
+package com.mobile.practice.customview.drawing
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * @author https://githu.com/asd3590058
+ *@fileName customview.drawing
+ * @date 2023/3/27 23:16
+ */
+class DashboardView(context: Context?, attributeSet: AttributeSet?) : View(context, attributeSet) {
+
+}
